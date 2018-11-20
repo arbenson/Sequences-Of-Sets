5bb60424b9fb0a5999b1c7515a7af8a5d49f09b3
Austin Benson <arbenson@gmail.com>
2018-11-20 13:25:25 -0500

    Update models for Julia 1.0

--- model_common.jl
+++ model_common.jl
@@ -1,12 +1,19 @@
+using Base.Threads
+using Combinatorics
+using FileIO
+using JLD2
+using LinearAlgebra
+using Printf
+
 # Minmum l2 projection onto the probability simplex. 
 # From Fig. 1 in https://stanford.edu/~jduchi/projects/DuchiShSiCh08.pdf
 function simplex_projection(v::Vector{Float64}, z::Float64=1.0,
                             minval::Float64=1e-8)
     μ = copy(v);
     sort!(μ, rev=true)
-    ρ = maximum(find((μ - (cumsum(μ) - z) ./ collect(1:length(μ))) .> 0))
-    θ = (sum(μ[1:ρ]) - z) / ρ
-    ret = max.(v - θ, 0)
+    ρ = maximum(findall((μ - (cumsum(μ) .- z) ./ collect(1:length(μ))) .> 0))
+    θ = (sum(μ[1:ρ]) .- z) / ρ
+    ret = max.(v .- θ, 0)
     ret = max.(ret, minval)
     return ret / sum(ret)
 end
